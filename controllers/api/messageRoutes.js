fda40d6af3fdad9df5f3bbee241129cff4ecfd8b
Brian Morse <brian.p.morse@gmail.com>
2023-05-03 14:35:03 -0700

    more work on message routes done

--- controllers/api/messageRoutes.js
+++ controllers/api/messageRoutes.js
@@ -1,16 +1,21 @@
 // necessary dependencies and models
-const passport = require('passport');
 const router = require('express').Router();
 const {Member, Message, Room, User} = require('../../models');
 
-//route to display messages from a given 
+//route to display messages from a given room
 router.get('/', async (req, res) => {
     try{
         const roomId = req.query.room_id;
         const messages = await Message.findall({
             where: {
                 roomId: roomId
-            }
+            },
+            include: {
+                model: User,
+                attributes: ['userName']
+            }, 
+            attributes: ['content', 'sent_at'],
+            order: [['sent_at', 'ASC']]        
         });
         res.status(200).json(messages)
     }
@@ -18,3 +23,8 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//route to post message in a given room
+router.post('/', passport.authenticate('jwt', {session: false}) async (req, res) => {
+    try{
+        const newMessage = await Message.create(req.body)
