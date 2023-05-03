65eaf5815f7c832f060c507ba9a3699971f8b328
Brian Morse <brian.p.morse@gmail.com>
2023-05-03 10:13:41 -0700

    adjusted message get route to display messages with matching room ID's only.

--- controllers/api/messageRoutes.js
+++ controllers/api/messageRoutes.js
@@ -1,9 +1,17 @@
+// necessary dependencies and models
+const passport = require('passport');
 const router = require('express').Router();
 const {Member, Message, Room, User} = require('../../models');
 
+//route to display messages from a given 
 router.get('/', async (req, res) => {
     try{
-        const messages = await Message.findall();
+        const roomId = req.query.room_id;
+        const messages = await Message.findall({
+            where: {
+                roomId: roomId
+            }
+        });
         res.status(200).json(messages)
     }
     catch (err) {
