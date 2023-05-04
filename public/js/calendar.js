b868f30cf7031f247b59b650e2180df4991dd0fc
Monique <mo.sherred@gmail.com>
2023-05-04 12:07:47 -0700

    im trying to get the calendar to appear

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var calendarEl = document.getElementById('calendar');
+document.addEventListener("DOMContentLoaded", function () {
+	var calendarEl = document.getElementById("calendar");
 
 	if (calendarEl) {
 		var calendar = new FullCalendar.Calendar(calendarEl, {
 			plugins: [FullCalendar.DayGridPlugin],
-			initialView: 'dayGridMonth',
+			initialView: "dayGridMonth",
 			headerToolbar: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'dayGridMonth,timeGridWeek,timeGridDay'
-			}
+				left: "prev,next today",
+				center: "title",
+				right: "dayGridMonth,timeGridWeek,timeGridDay",
+			},
 		});
 
 		calendar.render();
--- routes/projects.js
+++ routes/projects.js
@@ -2,20 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/projects', (req, res) => {
-	const projectsData = {
-		projects: [
-			// Example data, replace this with your actual data
-			{
-				name: 'Project 1',
-				files: ['file1', 'file2']
-			},
-			{
-				name: 'Project 2',
-				files: ['file3', 'file4']
-			}
-		]
-	};
-	res.render('projects', projectsData);
+	res.send('Projects page');
 });
 
 module.exports = router;
