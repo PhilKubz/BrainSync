e22267759298a41152d9d07c41c5c6c4b7fd1b51
KlosheKopa <richardsa@my.lanecc.edu>
2023-05-09 18:14:19 -0700

    Final fixes

--- controllers/communicationRoutes.js
+++ controllers/communicationRoutes.js
@@ -7,7 +7,7 @@ const {QueryTypes} = require('sequelize');
 
 router.get('/:id', withAuth, async (req, res) => {
     try {
-        const messages = await sequelize.query(`SELECT message.id, message.content, message.sent_at, user.userName, room.name FROM message JOIN room ON room.id = message.room_id JOIN user ON user.id = message.author_id WHERE message.room_id = ${req.params.id} ORDER BY message.sent_at ASC`, {type: QueryTypes.SELECT});
+        const messages = await sequelize.query(`SELECT message.id, message.content, message.sent_at, user.userName, room.name FROM message JOIN room ON room.id = message.room_id JOIN user ON user.id = message.author_id WHERE message.room_id = ${req.params.id} ORDER BY message.sent_at DESC`, {type: QueryTypes.SELECT});
         const roomDetails = await Room.findByPk(req.params.id);
         const details = roomDetails.get({ plain: true});
 
